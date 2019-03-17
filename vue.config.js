1a830a177889248e3400794cc8eb5fff55b545d5
hungnv132 <hungnv132@gmail.com>
2019-03-17 15:44:05 +0700

    remove outputDir properties

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ var BundleTracker  = require('webpack-bundle-tracker');
 
 module.exports = {
     indexPath: 'stock_dashboard.html',
-    outputDir: '/home/hnv132/Projects/thesite/mysite/static',
     publicPath: 'static/',
     configureWebpack: {
         plugins: [
